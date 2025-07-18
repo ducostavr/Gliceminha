fdceeba2d697e78e9771b1db6690d8b2c4587710
Saborevida <ometodovap@gmail.com>
2025-07-18 13:15:53 -0300

    Instalação do plugin Pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 1. Importamos o plugin do PWA
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [react()],
+  // 2. Adicionamos o plugin do PWA à lista
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Gliceminha',
+        short_name: 'Gliceminha',
+        description: 'Seu assistente para monitoramento de glicose.',
+        theme_color: '#667eea',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
